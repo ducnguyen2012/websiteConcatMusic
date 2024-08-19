87d5be5f72b9c6cd66f71ff143eeba0d4cdb28ec
ducnguyen2012 <166124443+ducnguyen2012@users.noreply.github.com>
2024-08-19 13:25:57 +0100

    Update UploadPage.jsx

--- src/Page/UploadPage.jsx
+++ src/Page/UploadPage.jsx
@@ -1,74 +1,4 @@
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// export function UploadPage() {
-//   const [selectedFiles, setSelectedFiles] = useState(null);
-//   const navigate = useNavigate()
-  
-//   const handleFileChange = (event) => {
-//     const files = event.target.files;
-    
-//     for (let i = 0; i < files.length; ++i) {
-//       const file = files[i]
-//       if (!file.name.endsWith('.mp3') && !file.name.endsWith('.wav')) {
-//         alert("ONLY .mp3 or .wav files ARE ALLOWED!")
-//         event.target.value = null;
-//         setSelectedFiles(null);
-//         return;
-//       }
-//     }
-//     setSelectedFiles(files);
-    
-//   };
 
-//   const handleUpload = async (event) => {
-    
-//     event.preventDefault();
-//     if (setSelectedFiles.length === 0)
-//       {
-//         alert("please select files and upload!")
-//         return;
-//       } 
-
-//     const formData = new FormData();
-//     Array.from(selectedFiles).forEach(file => {
-//         formData.append("files", file);    
-//     });
-    
-
-//     try {
-//       const response = await fetch("http://localhost:5000/upload", {
-//         method: "POST",
-//         body: formData,
-//       });
-
-//       if (response.ok) {
-//         alert("all the files have been upload and processing!")
-//         console.log("File uploaded successfully!");
-        
-//       } else {
-//         console.error("File upload failed.");
-//       }
-//     } catch (error) {
-//       return <div>
-//         <h1>WARNING: THERE IS NO FILE FOR UPLOAD!</h1>  
-//       </div>
-//       console.error("Error uploading file:", error);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <form onSubmit={handleUpload}>
-//         <input type="file" id="fileInput" multiple onChange={handleFileChange} />
-//         <button type="submit">Upload</button>
-//       </form>
-//       {selectedFiles && <p>Selected file: {selectedFiles.name}</p>}
-//       <a href="http://localhost:3000/DownloadPage">WARNING: AFTER UPLOAD YOUR FILE, CLICK TO UPLOAD AND GO TO DOWNLOAD PAGE WITH THIS LINK</a>
-//     </div>
-//   );
-// }
-
-// export default UploadPage
 
 
 import React, { useState } from "react";
@@ -137,7 +67,8 @@ export function UploadPage() {
       {selectedFiles.length > 0 && (
         <p>Selected files: {Array.from(selectedFiles).map(file => file.name).join(', ')}</p>
       )}
-      <a href="http://ducnguyen2012.github.io/DownloadPage">WARNING: AFTER UPLOAD YOUR FILE, CLICK TO UPLOAD AND GO TO DOWNLOAD PAGE WITH THIS LINK</a>
+{/*       <a href="http://ducnguyen2012.github.io/DownloadPage">WARNING: AFTER UPLOAD YOUR FILE, CLICK TO UPLOAD AND GO TO DOWNLOAD PAGE WITH THIS LINK</a> */}
+      <button onClick={navigate('/downloadPage')}>Go to downloadPage</button>
     </div>
   );
 }
